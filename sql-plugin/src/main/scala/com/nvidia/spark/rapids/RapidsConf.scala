d72530ad7d8f03437010d463cd32885ddaaf7cfb
ustcfy <96854327+ustcfy@users.noreply.github.com>
2025-03-26 09:54:53 +0800

    Add auto mapping of UDFs to GPU implementations (#94)
    Closes https://github.com/HighPerfDataAccelerator/spark-rapids/issues/53
    
    This PR adds UDF auto mapping. When the configuration is enabled, it
    automatically replaces `bytedance` in the package name with
    `bytedance.rapids`.
    
    For example, I renamed the
    [StringWordCount](https://github.com/NVIDIA/spark-rapids-examples/blob/main/examples/UDF-Examples/RAPIDS-accelerated-UDFs/src/main/java/com/nvidia/spark/rapids/udf/hive/StringWordCount.java)
    function to `com.bytedance.StringWordCount`, and then copied it to
    another UDF: `com.bytedance.rapids.StringWordCount`, adding an extra
    line: `System.out.println("New mapped UDF is running.");`.
    
    When running the following code, I check the logs and see: `"New mapped
    UDF is running."`
    ``` scala
    from pyspark.sql.types import *
    schema = StructType([
        StructField("c1", StringType()),
        StructField("c2", IntegerType()),
    ])
    data = [
        ("a b c d",1),
        ("",2),
        (None,3),
        ("the quick brown fox jumped over the lazy dog",3),
    ]
    df = spark.createDataFrame(
            SparkContext.getOrCreate().parallelize(data, numSlices=2),
            schema)
    df.createOrReplaceTempView("tab")
    // set config
    spark.conf.set("spark.rapids.sql.udf.autoMapping.enabled", True)
    // register UDF sql
    spark.sql("CREATE TEMPORARY FUNCTION {} AS '{}'".format("wordcount", "com.bytedance.StringWordCount"))
    // run UDF
    spark.sql("select c1, wordcount(c1) from tab").show()
    ```
    This demonstrates that the auto mapping functionality is working as
    intended.
    
    ---------
    
    Signed-off-by: ustcfy <fengyan_@mail.ustc.edu.cn>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -1087,6 +1087,12 @@ val GPU_COREDUMP_PIPE_PATTERN = conf("spark.rapids.gpu.coreDump.pipePattern")
     .booleanConf
     .createWithDefault(false)
 
+  val ENABLE_UDF_AUTO_MAPPING = conf("spark.rapids.sql.udf.autoMapping.enabled")
+    .doc("When set to true, automatically maps UDFs to GPU implementations according to some " +
+      "rules.")
+    .booleanConf
+    .createWithDefault(false)
+
   val PARQUET_READER_TYPE = conf("spark.rapids.sql.format.parquet.reader.type")
     .doc("Sets the Parquet reader type. We support different types that are optimized for " +
       "different environments. The original Spark style reader can be selected by setting this " +
@@ -3371,6 +3377,8 @@ class RapidsConf(conf: Map[String, String]) extends Logging {
   lazy val getSparkGpuResourceName: String = get(SPARK_GPU_RESOURCE_NAME)
 
   lazy val isCpuBasedUDFEnabled: Boolean = get(ENABLE_CPU_BASED_UDF)
+
+  lazy val isUDFAutoMappingEnabled: Boolean = get(ENABLE_UDF_AUTO_MAPPING)
 
   lazy val isFastSampleEnabled: Boolean = get(ENABLE_FAST_SAMPLE)
 

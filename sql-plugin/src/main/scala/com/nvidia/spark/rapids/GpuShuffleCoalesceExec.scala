54488fc998c5a7fc7492ea84f4f5ee156b773724
Hongbin Ma (Mahone) <mahongbin@apache.org>
2025-06-06 16:30:27 +0800

    op time should include the time acquiring semaphore, but this commit only fix some
    Signed-off-by: Hongbin Ma (Mahone) <mahongbin@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuShuffleCoalesceExec.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuShuffleCoalesceExec.scala
@@ -565,12 +565,14 @@ class GpuShuffleCoalesceIterator(iter: Iterator[CoalescedHostResult],
         iter.next()
       }
       withResource(hostCoalescedResult) { _ =>
-        // We acquire the GPU regardless of whether `hostConcatResult`
-        // is an empty batch or not, because the downstream tasks expect
-        // the `GpuShuffleCoalesceIterator` to acquire the semaphore and may
-        // generate GPU data from batches that are empty.
-        GpuSemaphore.acquireIfNecessary(TaskContext.get())
-        GpuMetric.ns(opTimeMetric)(hostCoalescedResult.toGpuBatch(dataTypes))
+        GpuMetric.ns(opTimeMetric)(
+          // We acquire the GPU regardless of whether `hostConcatResult`
+          // is an empty batch or not, because the downstream tasks expect
+          // the `GpuShuffleCoalesceIterator` to acquire the semaphore and may
+          // generate GPU data from batches that are empty.
+          GpuSemaphore.acquireIfNecessary(TaskContext.get())
+          hostCoalescedResult.toGpuBatch(dataTypes)
+        )
       }
     }
   }

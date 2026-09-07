7cf586df469333e87b9afd9e701734da989d5672
Hongbin Ma <mahongbin@apache.org>
2026-09-07 02:27:36 +0000

    Test aggregate measure pushdown optimizer rule
    Signed-off-by: Hongbin Ma <mahongbin@apache.org>

---
+++ tests/src/test/scala/com/nvidia/spark/rapids/GpuPushAggregateMeasureBeforeJoinSuite.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2026, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nvidia.spark.rapids
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.catalyst.plans.logical.Aggregate
+import org.apache.spark.sql.functions.{col, expr, max, sum}
+
+class GpuPushAggregateMeasureBeforeJoinSuite extends SparkQueryCompareTestSuite {
+
+  private val enabledKey =
+    "spark.rapids.sql.optimizer.pushAggregateMeasureBeforeJoin.enabled"
+
+  private def conf(enabled: Boolean): SparkConf = new SparkConf()
+    .set(enabledKey, enabled.toString)
+    .set("spark.sql.autoBroadcastJoinThreshold", "-1")
+
+  private def distributiveSumQuery(spark: org.apache.spark.sql.SparkSession): DataFrame = {
+    val measures = spark.range(0, 12)
+      .selectExpr(
+        "id % 4 AS join_key",
+        "CAST(id + 1 AS DOUBLE) AS price",
+        "CAST((id % 3) / 10.0 AS DOUBLE) AS discount")
+      .where("price > 0")
+    val duplicatedKeys = spark.range(0, 4)
+      .selectExpr("id AS other_key")
+      .union(spark.range(0, 2).selectExpr("id AS other_key"))
+
+    measures
+      .join(duplicatedKeys, col("join_key") === col("other_key"))
+      .groupBy("join_key")
+      .agg(sum(expr("price * (1.0 - discount)")).as("revenue"))
+  }
+
+  private def materializationOnlyQuery(
+      spark: org.apache.spark.sql.SparkSession): DataFrame = {
+    val measures = spark.range(0, 12)
+      .selectExpr(
+        "id % 4 AS join_key",
+        "CAST(id + 1 AS DOUBLE) AS price",
+        "CAST((id % 3) / 10.0 AS DOUBLE) AS discount")
+      .where("price > 0")
+    val keys = spark.range(0, 4).selectExpr("id AS other_key")
+
+    measures
+      .join(keys, col("join_key") === col("other_key"), "left_outer")
+      .groupBy("join_key")
+      .agg(sum(expr("price * (1.0 - discount)")).as("revenue"))
+  }
+
+  private def compositeSumWithOtherMeasure(
+      spark: org.apache.spark.sql.SparkSession): DataFrame = {
+    val measures = spark.range(0, 12)
+      .selectExpr(
+        "id % 4 AS join_key",
+        "CAST(id + 1 AS DOUBLE) AS price",
+        "CAST((id % 3) / 10.0 AS DOUBLE) AS discount")
+      .where("price > 0")
+    val keys = spark.range(0, 4).selectExpr("id AS other_key")
+
+    measures
+      .join(keys, col("join_key") === col("other_key"))
+      .groupBy("join_key")
+      .agg(
+        sum(expr("price * (1.0 - discount)")).as("revenue"),
+        max("price").as("max_price"))
+  }
+
+  private def normalized(rows: Array[Row]): Seq[String] = rows.map(_.toString).sorted.toSeq
+
+  test("pre-aggregate a distributive SUM before an inner join") {
+    var expected = Seq.empty[String]
+    withCpuSparkSession(spark => {
+      expected = normalized(distributiveSumQuery(spark).collect())
+    }, conf(enabled = false))
+
+    withCpuSparkSession(spark => {
+      val query = distributiveSumQuery(spark)
+      val optimized = query.queryExecution.optimizedPlan
+      val aggregates = optimized.collect { case aggregate: Aggregate => aggregate }
+
+      assert(normalized(query.collect()) === expected)
+      assert(aggregates.size >= 2, optimized.treeString)
+      assert(optimized.treeString.contains("_rapids_measure_"), optimized.treeString)
+      assert(aggregates.exists(_.groupingExpressions.exists(_.toString.contains("join_key"))),
+        optimized.treeString)
+    }, conf(enabled = true))
+  }
+
+  test("do not pre-aggregate through an outer join") {
+    withCpuSparkSession(spark => {
+      val optimized = materializationOnlyQuery(spark).queryExecution.optimizedPlan
+      val aggregates = optimized.collect { case aggregate: Aggregate => aggregate }
+
+      assert(aggregates.size === 1, optimized.treeString)
+      assert(optimized.treeString.contains("_rapids_measure_"), optimized.treeString)
+    }, conf(enabled = true))
+  }
+
+  test("do not rewrite when another aggregate uses a measure input") {
+    withCpuSparkSession(spark => {
+      val optimized = compositeSumWithOtherMeasure(spark).queryExecution.optimizedPlan
+
+      assert(!optimized.treeString.contains("_rapids_measure_"), optimized.treeString)
+    }, conf(enabled = true))
+  }
+}
